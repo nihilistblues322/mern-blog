11221617e4ebfb539740ec19df75df20b0936a5f
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-05-04 23:35:49 +0500

    mongoose connect

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+    .connect(
+        "mongodb+srv://msc:boss555@cluster0.07mhw54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => {
+        console.log("DB OK");
+    })
+    .catch((err) => {
+        console.log("DB ERROR", err);
+    });
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello");
+});
+
+app.post("/auth/login", (req, res) => {
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: "Vasya",
+        },
+        "secret123"
+    );
+
+    res.json({
+        success: true,
+        token,
+    });
 });
 
 app.listen(4444, (err) => {
