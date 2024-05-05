2644dc708911c11d4b3baf00a6b59ea28526cf2e
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-05-05 21:03:34 +0500

    db user

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
 
 mongoose
     .connect(
@@ -18,12 +20,24 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.json({ success: true });
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+    const user = await doc.save();
+
+    res.json({ user });
 });
 
 app.listen(4444, (err) => {
