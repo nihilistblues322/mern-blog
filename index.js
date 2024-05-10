a6682bd1789f5a81385f5cc659dcd3a525630723
iuiuiuiuuiui <144712490+nihilistblues322@users.noreply.github.com>
2024-05-10 16:44:24 +0500

    backend ready

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import {
 import checkAuth from "./utils/checkAuth.js"
 import * as UserController from "./controllers/UserController.js"
 import * as PostController from "./controllers/PostController.js"
+import handleValidationErrors from "./utils/handleValidationErrors.js"
 
 mongoose
     .connect(
@@ -36,8 +37,19 @@ const upload = multer({ storage })
 app.use(express.json())
 app.use("/uploads", express.static("uploads"))
 
-app.post("/auth/login", loginValidation, UserController.login)
-app.post("/auth/register", registerValidation, UserController.register)
+app.post(
+    "/auth/login",
+    loginValidation,
+    handleValidationErrors,
+    UserController.login
+)
+app.post(
+    "/auth/register",
+    registerValidation,
+    handleValidationErrors,
+    handleValidationErrors,
+    UserController.register
+)
 app.get("/auth/me", checkAuth, UserController.getMe)
 
 app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
@@ -48,8 +60,20 @@ app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
 
 app.get("/posts", PostController.getAll)
 app.get("/posts/:id", PostController.getOne)
-app.post("/posts", checkAuth, postCreateValidation, PostController.create)
-app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update)
+app.post(
+    "/posts",
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.create
+)
+app.patch(
+    "/posts/:id",
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.update
+)
 app.delete("/posts/:id", checkAuth, PostController.remove)
 
 app.listen(4444, (err) => {
